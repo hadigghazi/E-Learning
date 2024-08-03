4c98398075cc50aab25bc801b7d66ab43bdf2c81
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-03 20:12:38 +0300

    Adding the course model

---
+++ backend/models/Course.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const CourseSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Course = mongoose.model('Course', CourseSchema);
+export default Course;
