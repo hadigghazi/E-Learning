2525001c0fe703bd6fd54b375241da20b2e1bf2d
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-04 07:14:49 +0300

    Adding the get course

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -26,39 +26,3 @@ export const getCourse = catchAsync(async (req, res, next) => {
   });
 });
 
-export const createCourse = catchAsync(async (req, res, next) => {
-  const newCourse = await Course.create(req.body);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      course: newCourse
-    }
-  });
-});
-
-export const updateCourse = catchAsync(async (req, res, next) => {
-  const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true
-  });
-  if (!course) {
-    return next(new AppError('No course found with that ID', 404));
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      course
-    }
-  });
-});
-
-export const deleteCourse = catchAsync(async (req, res, next) => {
-  const course = await Course.findByIdAndDelete(req.params.id);
-  if (!course) {
-    return next(new AppError('No course found with that ID', 404));
-  }
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
-});
