22c02939a7a397f31d2de26db9bce2854f6ef57c
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-04 19:16:54 +0300

    Adding the navbar to the app

--- admin-app/app/src/App.js
+++ admin-app/app/src/App.js
@@ -1,12 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProtectedRoute from './components/ProtectedRoute';
+import WelcomePage from './pages/WelcomePage';
+import Navbar from './components/Navbar';
+import CoursesPage from './pages/CoursesPage';
+import CourseDetailPage from './pages/CourseDetailPage';
+import MyCoursesPage from './pages/MyCoursesPage';
 
-function App() {
-  return (
-    <div className="App">
-     hello
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Navbar />
+
+            <Routes>
+              <Route path='/' element={<WelcomePage />} />
+            <Route
+          path="/login"
+          element={
+            <ProtectedRoute>
+              <LoginPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <ProtectedRoute>
+              <RegisterPage />
+            </ProtectedRoute>
+          }
+        />
+                <Route path='/courses' element={<CoursesPage />} />
+                <Route path="/courses/:id" element={<CourseDetailPage />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
