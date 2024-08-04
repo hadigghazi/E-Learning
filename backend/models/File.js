6fafae7166459ba83db952408852051e7be87e50
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-04 10:52:12 +0300

    Updating the file model

--- backend/models/File.js
+++ backend/models/File.js
@@ -1,18 +1,27 @@
 import mongoose from 'mongoose';
 
 const FileSchema = new mongoose.Schema({
+    fileName: {
+        type: String,
+        required: [true, 'File must have a name']
+    },
+    filePath: {
+        type: String,
+        required: [true, 'File must have a path']
+    },
+    fileType: {
+        type: String,
+        required: [true, 'File must have a type']
+    },
+    uploadedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'File must be associated with a user']
+    },
     courseId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Course',
-        required: true
-    },
-    fileName: {
-        type: String,
-        required: true
-    },
-    fileUrl: {
-        type: String,
-        required: true
+        required: [true, 'File must be associated with a course']
     },
     uploadedAt: {
         type: Date,
