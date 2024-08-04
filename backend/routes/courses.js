d5012d0e95eb087a8d9f537771c8711e9ac2a857
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-04 07:15:49 +0300

    Adding the update course

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -35,3 +35,19 @@ export const createCourse = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+export const updateCourse = catchAsync(async (req, res, next) => {
+  const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+  if (!course) {
+    return next(new AppError('No course found with that ID', 404));
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      course
+    }
+  });
+});
