d46fd0566b85428cd3fcf7b72ab2f04c0aa0e043
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-04 07:16:08 +0300

    Adding the delete course

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -51,3 +51,14 @@ export const updateCourse = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+export const deleteCourse = catchAsync(async (req, res, next) => {
+  const course = await Course.findByIdAndDelete(req.params.id);
+  if (!course) {
+    return next(new AppError('No course found with that ID', 404));
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
